5b5eb530995f3de241b4120ba3615a43c5f81778
Eduardo Rodrigues De Lara <eduardo_r_lara@estudante.sesisenai.org.br>
2026-08-13 18:02:30 +0000

    fiz a atividade 9

---
+++ exercicio9.py
@@ -0,0 +1,25 @@
+class Livro:
+    def __init__(self, titulo: str, autor: str, paginas: int):
+        self.titulo = titulo
+        self.autor = autor
+        self.paginas = paginas
+
+    def comparar_tamanho(self, outro_livro):
+            if self.paginas > outro_livro.paginas:
+                print(f"O livro {self.titulo} possui mais paginas doque {outro_livro.titulo}.")
+            elif self.paginas < outro_livro.paginas:
+                print(f"O livro {outro_livro.titulo} possui mais paginas doque {self.titulo}.")
+            elif self.paginas == outro_livro.paginas:
+                print(f"Os dois livros possuem a mesma quantidade de paginas.")
+        
+
+    def __str__(self):
+        return f"Livro: {self.titulo}, {self.paginas} paginas pelo Autor {self.autor}"
+
+
+livro_1 = Livro("Demon Slayer Volume 17", "Koyoharu Gotouge", 192)
+livro_2 = Livro("O Pequeno Principe", "Antoine de Saint-Exupéry", 140)
+
+print(livro_1)
+print(livro_2)
+livro_1.comparar_tamanho(livro_2)
